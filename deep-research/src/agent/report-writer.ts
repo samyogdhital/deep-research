4c6c04d0308c151b85afa54f7010c0c0d31d17c8
rameshdon1 <139542777+rameshdon1@users.noreply.github.com>
2025-02-25 21:43:50 +0545

    added agents folder on backend.

---
+++ deep-research/src/agent/report-writer.ts
@@ -0,0 +1,7 @@
+// IMPORTANT: Do not remove the comment at all.
+// Step 1: All the learnings from information crunching agent is passed here with detailed objective. The user's iniital query is also passed here for detailed report writing based on the user's query. Answering the user's percise query with to the point information.
+
+// Step 2: The reponse from initial-report-agent is sent here along with all the learnings from information crunching agent and we include all the infomration missed by initial-report-agent based on the objective we have and the user demands.
+// We need to make sure that the information missed by initial-report-agent is included is caught here and more detailed and comprehensive report is presented for final-report-agent.
+
+// Step 3: Report from middle-report-agent is passed here along with all the learnings and conclusions we got and made during the deep research process. The report from both of these initial and middle report agent is also passed here. The user's initial query is also passed here and made sure that this final report is not missing any important infomration if it is present on the internet answering the precise question the user has asked.
